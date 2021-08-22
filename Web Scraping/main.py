b88b25e406527a5be6422fc8afb15fed9d2b9aee
Vivek Singh <viveksingh1936@gmail.com>
2021-08-22 16:58:17 +0530

    22/08/2021
    Local html scraping

---
+++ Web Scraping/main.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+
+with open ('D:\DE\Web Scraping\home.html', 'r') as html_file:
+    content = html_file.read()
+    
+    soup = BeautifulSoup(content, 'lxml')
+    course_cards = soup.find_all('div', class_ = 'card')
+
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]  
+
+        #using f string
+        
+        print(f'{course_name} costs {course_price}')
+    
