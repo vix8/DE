517dd891d2920b4634c450774740734dc6069729
Vivek Singh <viveksingh1936@gmail.com>
2021-08-22 23:06:39 +0530

    22/08/2021
    Box Office scraping

--- Web Scraping/main.py
+++ Web Scraping/main.py
@@ -1,11 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.imdb.com/chart/toptv/').text
+html_text = requests.get('https://www.boxofficemojo.com/chart/top_lifetime_gross/?area=XWW').text
 
 soup = BeautifulSoup(html_text, 'lxml')
-movies = soup.find('td','titleColumn').text.replace('      ', '').replace('\n' , ' ').strip()
-rating = soup.find('td', class_ = 'ratingColumn imdbRating').text
 
+rank = soup.find('td','a-text-right mojo-header-column mojo-truncate mojo-field-type-rank').text
+movies = soup.find('td','a-text-left mojo-field-type-title').text
+gross = soup.find('td', class_ = 'a-text-right mojo-field-type-money').text
+year = soup.find('td','a-text-left mojo-field-type-year').text
+
+print(rank)
 print(movies)
-print(rating)
+print(gross)
+print(year)
